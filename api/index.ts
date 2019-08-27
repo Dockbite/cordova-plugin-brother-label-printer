a24d9164ffcbf369a6803f36d35b698edfd85a12
Kester Everts <kester.everts@dockbite.nl>
2019-08-27 15:10:10 +0200

    First new version

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,21 @@
 import * as cordovaPrinterApi from '../www/printer';
 
-export type PaperLabel = 'W17H54'|'W17H87'|'W23H23'|'W29H42'|'W29H90'|'W38H90'|'W39H48'|'W52H29'|'W62H29'|'W62H100'|'W12'|'W29'|'W38'|'W50'|'W54'|'W62'|'W60H86'|'W54H29'|'W62RB'
+export type PaperLabel =
+  'W17H54'|'W17H87'|'W23H23'|'W29H42'|
+  'W29H90'|'W38H90'|'W39H48'|'W52H29'|
+  'W62H29'|'W62H100'|'W12'|'W29'|'W38'|
+  'W50'|'W54'|'W62'|'W60H86'|'W54H29'|'W62RB';
 
 export interface Printer {
   model: string // Usually of the form 'QL_720NW' on Android
-  port: 'NET' | 'BLUETOOTH'
-  modelName: string // Usually of the form 'Brother QL-720NW'
-  ipAddress?: string
-  macAddress?: string
-  serialNumber?: string
-  nodeName?: string
-  location?: string
-  paperLabelName?: PaperLabel
+  port: 'NET' | 'BLUETOOTH';
+  modelName: string; // Usually of the form 'Brother QL-720NW'
+  ipAddress?: string;
+  macAddress?: string;
+  serialNumber?: string;
+  nodeName?: string;
+  location?: string;
+  paperLabelName?: PaperLabel;
 }
 
 export function findNetworkPrinters(): Promise<Printer[]> {
@@ -26,7 +30,7 @@ export function setPrinter(printer: Printer, paperLabel: PaperLabel): Promise<vo
   });
 }
 
-export function printBitmap(base64Image: string): Promise<void> {
+export function printLabel(base64Image: string): Promise<void> {
   return new Promise((resolve, reject) => {
     cordovaPrinterApi.printViaSDK(base64Image, resolve, reject);
   });
